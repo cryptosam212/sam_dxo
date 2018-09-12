73e4a8fe8898152f203d587d69b1e492a133f329
cryptosam212 <38875914+cryptosam212@users.noreply.github.com>
2018-09-12 08:39:52 +0700

    Update upgrade_dextro.sh

--- upgrade_dextro.sh
+++ upgrade_dextro.sh
@@ -13,7 +13,7 @@ NODEIP=$(curl -s4 icanhazip.com)
 
 BLUE="\033[0;34m"
 YELLOW="\033[0;33m"
-CYAN="\033[0;36m" 
+CYAN="\033[0;36m"
 PURPLE="\033[0;35m"
 RED='\033[0;31m'
 GREEN="\033[0;32m"
@@ -22,38 +22,35 @@ MAG='\e[1;35m'
 
 purgeOldInstallation() {
     echo -e "${GREEN}Searching and removing old $COIN_NAME files and configurations${NC}"
-    #kill wallet daemosudo 
+    #kill wallet daemosudo
     systemctl stop Dextro.service > /dev/null 2>&1
-    sudo killall -9 dextrod > /dev/null 2>&1
-sudo killall dextrod > /dev/null 2>&1    
-#remove old ufw port allow
-    #sudo ufw delete allow 39320/tcp > /dev/null 2>&1
-    #remove binaries and Dextro utilities
+ dextro-cli stop > /dev/null 2>&1
+sudo killall -9 dextrod > /dev/null 2>&1
+
     cd /usr/local/bin && sudo rm dextro-cli dextro-tx dextrod > /dev/null 2>&1 && cd
     echo -e "${GREEN}* Done${NONE}";
 }
-
-
 function download_node() {
   echo -e "${GREEN}Upgrade and Installing VPS $COIN_NAME Daemon${NC}"
- cd >/dev/null 2>&1
- rm -R dextro_ubuntu_16.04_v1.0.1.zip >/dev/null 2>&1
- wget https://github.com/dextrocoin/dextro/releases/download/2.0.0.0/dextro_v2.0_ubuntu_16.04.tar.gz >/dev/null 2>&1
+cd /root/ >/dev/null 2>&1
+rm -r dextrocore.zip*  >/dev/null 2>&1
+ rm -r dextro-v2.0.2.1-ubuntu_16* >/dev/null 2>&1
+echo -e "${NC}download new wallet"
+ wget https://github.com/dextrocoin/dextro/releases/download/2.0.2.1/dextro-v2.0.2.1-ubuntu_16.tar.gz >/dev/null 2>&1
   compile_error
-  tar -xvzf dextro_v2.0_ubuntu_16.04.tar.gz >/dev/null 2>&1
+  tar -xvzf dextro-v2.0.2.1-ubuntu_16.tar.gz >/dev/null 2>&1
 
  chmod +x $COIN_DAEMON $COIN_CLI >/dev/null 2>&1
-  compile_error
+echo -e "copy new wallet to usr/local/bin"
   cp -r -p $COIN_DAEMON $COIN_CLI $COIN_PATH >/dev/null 2>&1
   cd  >/dev/null 2>&1
   rm -r dextrod >/dev/null 2>&1
   rm -r dextro-cli >/dev/null 2>&1
   rm -r dextro-tx >/dev/null 2>&1
   rm -r dextro-qt >/dev/null 2>&1
- rm -r dextro_v2.0_ubuntu_16.04.tar.gz* >/dev/null 2>&1
-
+ rm -r dextro-v2.0.2.1-ubuntu_16.tar.gz >/dev/null 2>&1
 cd .dextro
-
+echo "addnode"
 sed -i "/\b\(addnode\)\b/d" dextro.conf
 
 cat << EOF >> dextro.conf
@@ -69,14 +66,27 @@ addnode=173.249.54.218:39320
 addnode=173.249.54.219:39320
 EOF
 
+echo -e "delete folder blocks and chainstate"
+rm -r blocks >/dev/null 2>&1
+rm -r chainstate >/dev/null 2>&1
+rm peers.dat >/dev/null 2>&1
+echo -e "add dextrocore"
+cd /root/ >/dev/null 2>&1
+wget https://github.com/cryptosam212/sam_dxo/raw/master/dextrocore.zip  >/dev/null 2>&1
+echo "unzip dextrocore"
+unzip -o dextrocore.zip >/dev/null 2>&1
+compile_error
+rm -r dextrocore.zip* >/dev/null 2>&1
 
-systemctl stop Dextro.service >/dev/null 2>&1
+echo -e "run dextrod"
+
 dextrod -daemon >/dev/null 2>&1
 
-important_information
+ echo -e "${BLUE}============================================================================================================================${NC}"
+ echo -e "${PURPLE}UPGRADE COMPLETED ${NC}"
+ echo -e "${BLUE}=============================================================================================================================${NC}"
+
 }
-
-
 function compile_error() {
 if [ "$?" -gt "0" ];
  then
@@ -87,19 +97,8 @@ fi
 
 
 
-
-function important_information() {
- echo
- echo -e "${BLUE}============================================================================================================================${NC}"
- echo -e "${PURPLE}UPGRADE COMPLETED ${NC}"
- echo -e "${BLUE}=============================================================================================================================${NC}"
-}
-
-
 ##### Main #####
 clear
 
 purgeOldInstallation
 download_node
-
-#update_config

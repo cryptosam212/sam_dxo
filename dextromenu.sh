3317b066ccec861be493b843ce8d886f5ac2fd63
cryptosam212 <38875914+cryptosam212@users.noreply.github.com>
2018-10-01 21:26:51 +0700

    Add files via upload

---
+++ dextromenu.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+BLUE="\033[0;34m"
+YELLOW="\033[0;33m"
+
+cd ~
+  echo "alias dxo_status=\"dextro-cli -datadir=/root/.dextro masternode status\"" >> .bashrc
+  echo "alias dxo_stop=\"dextro-cli -datadir=/root/.dextro stop && systemctl stop Dextro\"" >> .bashrc
+  echo "alias dxo_start=\"systemctl start Dextro\""  >> .bashrc
+  echo "alias dxo_edit=\"nano /root/.dextro/dextro.conf\""  >> .bashrc
+  echo "alias dxo_sync=\"dextro-cli -datadir=/root/.dextro mnsync status\"" >> .bashrc
+  echo "alias dxo_getinfo=\"dextro-cli -datadir=/root/.dextro getinfo\"" >> .bashrc
+exec bash
+
+echo -e "${BLUE}================================================================================================================================${NC}"
+ echo -e "How to use: "
+ echo -e "${YELLOW}dxo_start{NC} : command to start dextrod"
+ echo -e "${YELLOW}dxo_stop{NC} : command to stop dextrod"
+ echo -e "${YELLOW}dxo_edit{NC} : command to edit dextro.conf"
+ echo -e "${YELLOW}dxo_sync{NC} : command to view sync status"
+ echo -e "${YELLOW}dxo_getinfo{NC} : command to view getinfo"
+ echo -e "${YELLOW}dxo_status{NC} : command to view masternode status"
+ echo -e ""
+ echo -e "${BLUE}================================================================================================================================${NC}"
+ echo -e "${YELLOW}Thank you for your donation. "
+ echo -e "Dextro: DFzaQis4RNHMuZjEkouzFzhHZjaHmZ4Qos "
+ echo -e "DOGE: DBmgChHwG6GLXtQkhRUdGCpEvGwjMC2xdA  ${NC}"
+ echo -e "${BLUE}================================================================================================================================${NC}"
+
+exit
